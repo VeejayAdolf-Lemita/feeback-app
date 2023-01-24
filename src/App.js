2ceed61da0ef5141be856af19b1f1a5d5ccef603
VeejayAdolf-Lemita <102731011+VeejayAdolf-Lemita@users.noreply.github.com>
2023-01-24 23:18:59 +0800

    add edit function

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import { v4 as uuidv4 } from 'uuid'
-import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackList from './components/FeedbackList'
 import FeedbackForm from './components/FeedbackForm'
-import FeedbackData from './data/feedbackData'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 
 function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
-    const deleteFeedback = (id) => {
-        if (window.confirm('Are you sure you want to delete?')){
-            setFeedback(feedback.filter((item)=> item.id !== id))
-        }
-    }
-
-    const addFeedback = (newFeedback) =>{
-        newFeedback.id = uuidv4()
-        // console.log(newFeedback)
-        setFeedback([newFeedback, ...feedback])
-    }
-
+ 
     return (
-        <>
+        <FeedbackProvider>
             <Header />
             <div  className="container">
-                <FeedbackForm handleAdd= {addFeedback} />
-                <FeedbackStats feedback={ feedback }/>
-                <FeedbackList feedback={ feedback } handleDelete={deleteFeedback}/>
+                <FeedbackForm  />
+                <FeedbackStats />
+                <FeedbackList />
             </div>    
-        </>
+        </FeedbackProvider>
     )
 }
 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackForm({handleAdd}) {
+function FeedbackForm() {
   const [text, setText] = useState('')
   const [rating, setRating] = useState()
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
+
+  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext)
+
+  useEffect(() => {
+    if(feedbackEdit.edit === true){
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
 
   const handleTextChange = (e) => {
 
@@ -33,10 +44,15 @@ function FeedbackForm({handleAdd}) {
       // rating:rating
       //Shorthand for ^ this
       text,
-      rating
+      rating,
     }
-    // console.log(newFeedback);
-    handleAdd(newFeedback);
+
+    if(feedbackEdit.edit === true){
+      updateFeedback(feedbackEdit.item.id, newFeedback)
+    } else {
+      addFeedback(newFeedback)
+    }
+
     setText('')
   }
  }
